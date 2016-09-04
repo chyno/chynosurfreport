17e60d128d05c82921a6e2077ded518083ae97b9
Maria Lopez <mane@johns-air-2.fios-router.home>
2016-09-03 23:09:10 -0400

    fix rewrite 4

--- server.js
+++ server.js
@@ -1,13 +1,70 @@
-var http = require('http');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var swig = require('swig');
+var api = require('./routes/api');
+var app = express();
+var webpackMiddleware;
+var webpack;
+var wpconfig;
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+
+/*
+if (app.get('env') == 'development') {
+  var browserSync = require('browser-sync');
+  var bs = browserSync({ logSnippet: false });
+  app.use(require('connect-browser-sync')(bs));
+}
+*/
+
+// view engine setup
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 9000);
+
+
+app.use('/', api);
+
+//var debug = require('debug')('aurelia-node');
+
+/*
+if ( app.get('env') === 'development')
+{
+   webpackMiddleware = require('webpack-dev-middleware');
+   webpack = require('webpack');
+   wpconfig = require('./webpack.config');
+   app.use(webpackMiddleware(webpack(wpconfig), {
+      publicPath: "/build",
+      headers: {"X-Custom_Webpack_Header": "yes" },
+      stats : { colors: true }
+   }
+   ));
+}
+*/
+
+var server = app.listen(app.get('port'), 
+
+ function (err) {
+
+  if (err) {
+    console.log('Error: ' + err);
+  }
+   debug('Express server listening on port ' + server.address().port);
+   console.log('Express server listening on port: ' + server.address().port);
 });
-var port = process.env.PORT || 3000;
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(port);
 
-// Put a friendly message on the terminal
-console.log(port);
+
+module.exports = app;
